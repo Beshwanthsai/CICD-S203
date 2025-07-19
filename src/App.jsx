e0558544233c9d4538607eb2800badb9e39e973f
Beshwanth Sai Katari <beshwanthsai2005@gmail.com>
2025-07-19 20:05:03 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/CICD-S203">
+    <Router basename="/CICD-S203/">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
